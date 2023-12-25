5b66a6874300ca2b7a471e7cd659bedf0ea36d0c
jalaaJ <abusalehjalaa1@gmail.com>
2023-12-25 12:46:59 +0200

    Created the material, installed the firebase and configured it

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const modules: any = [
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...modules
+  ],
+  exports: modules
+})
+export class MaterialModule { }

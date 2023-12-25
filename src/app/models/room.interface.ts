2e5d155c12070125967e16bbf21494d25baf5fc5
jalaaJ <abusalehjalaa1@gmail.com>
2023-12-26 00:11:35 +0200

    Created the chat service, and created the Models, whci hare the scemas of the database

---
+++ src/app/models/room.interface.ts
@@ -0,0 +1,7 @@
+import { IMessage } from "./";
+
+export interface IChatRoom {
+    id: string;
+    roomName: string;
+    messages: Array<IMessage>;
+}
--- src/app/models/user.interface.ts
+++ src/app/models/user.interface.ts
@@ -1,6 +1,6 @@
 export interface User {
-    uid: string,
-    email: string,
-    displayName: string,
-    photoURL: string
+    uid: string;
+    email: string;
+    displayName: string;
+    photoURL: string;
 }

5cb89dfc5b37d8016a50dbdd0326f91a211ef9da
jalaaJ <abusalehjalaa1@gmail.com>
2023-12-25 16:47:04 +0200

    updated the private helper functions in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import * as firebase from 'firebase/compat/app';
+import { User } from '../models/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +23,29 @@ export class AuthService {
 
   private async authLogin(provider: firebase.default.auth.AuthProvider) {
     const res = await this.afAuth.signInWithPopup(provider);
-    console.log(res);
+    this.setUserData(res.user as User);
+  }
+
+  private setUserData(user: User){
+    if(!user) return;
+
+    // Collection of users in the database
+    const userReference: AngularFirestoreDocument<User> = this.afstore.doc(
+      `users/${user.uid}`
+    )
+
+    const userData: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL
+    }
+
+    return userReference.set(userData, {
+
+      // We use it so we won't override the data, we append it
+      merge: true
+    })
   }
 
 }

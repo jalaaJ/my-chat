b89cb13eb019f42856eb248214bb8e5eb3652983
jalaaJ <abusalehjalaa1@gmail.com>
2023-12-26 00:00:31 +0200

    Added routing to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatContainerComponent } from './components/chat-container/chat-container.component';
+import { authGuard } from './guards/auth.guard';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "chat",
+    component: ChatContainerComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: "chat/:roomId",
+    component: ChatContainerComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "**",
+    redirectTo: ""
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

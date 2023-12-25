41b86afe10fc7fc2c57be6b4660f51b0f4ff7e5a
jalaaJ <abusalehjalaa1@gmail.com>
2023-12-25 14:57:01 +0200

    Added google signIn to my authService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
+import { FirestoreModule } from '@angular/fire/firestore'
 
 @NgModule({
   declarations: [
@@ -12,7 +17,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    FirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import * as firebase from 'firebase/compat/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afstore: AngularFirestore, private afAuth: AngularFireAuth) {}
+
+  public signInWithGoogle() {
+    this.authLogin(new firebase.default.auth.GoogleAuthProvider())
+  }
+
+  private authLogin(provider: firebase.default.auth.AuthProvider) {
+    return this.afAuth.signInWithPopup(provider).then(res => {
+      console.log(res);
+    })
+  }
+
+}
